2283706705d018792984b0df2a75fbb78c99eb05
deliq <60697188+deliq@users.noreply.github.com>
2020-03-30 14:25:09 +0200

    Add files via upload

---
+++ vue.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+    el: '#nahrada_txt',
+    data: {
+        text: 'Zvolil som alternativnu moznost urobenia tohoto projektu, pretoze pocas toho ako som pracoval na povodnom programe a snazil sa ho nainstalovat, nastalo mnoho chyb. A prave kvoli tomu som nebol schopny tento povodny projekt dokoncit',
+        text1: "I have chosen an alternative way of doing this project because there were lots of problems while working with the original program and trying to install it. And that's why I wasn't suppose to finish the original project properly.",
+        nadpis1: 'Nahradny projekt za povodny',
+        nadpis2: 'Substitute project of the original one',
+        svk: './SVK.jpg',
+        eng: './ENG.jpg',
+        jazyk: true,
+        vyber1: 'Iny sposob zmeny jazyka',
+        vyber2: 'Other way of changing language',
+        moznost: true,
+
+
+
+
+
+    }
+
+});
+
+//app.moznost = false;
